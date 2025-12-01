1e1c1b9325c045e6344fbb51c69844548c427faf
Saquib <101272076+syeedsaquib@users.noreply.github.com>
2025-12-01 22:35:19 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="torchbenchx",
     version="0.1.0",
     author="Syeed Mohd Saquib",
-    author_email="syeedmsaquib@example.com",
+    author_email="syeedmsaquib@gmail.com",
     description="Unified Deep Learning Benchmark Library for PyTorch and TensorFlow",
     long_description = Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
@@ -31,3 +31,4 @@ setup(
     ],
     python_requires=">=3.8",
 )
+
